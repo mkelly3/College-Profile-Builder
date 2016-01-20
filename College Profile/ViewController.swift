b4b44c69a2c2b12dd084924bd4e3ceb20eb69973
Student <student@mkelly2.local>
2016-01-20 12:04:09 -0600

    Allow for entries to be added

--- College Profile/ViewController.swift
+++ College Profile/ViewController.swift
@@ -11,14 +11,15 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var editButton: UIBarButtonItem!
     
     var cities: [Colleges] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
  
     }
-
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return cities.count
     }
@@ -28,7 +29,28 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         cell.textLabel?.text = cities[indexPath.row].name
         return cell
     }
-
-
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            cities.removeAtIndex(indexPath.row)
+            tableView.reloadData()
+        }
+    }
+    
+    @IBAction func onTappedPlusButton(sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Add City", message: nil, preferredStyle: .Alert)
+        alert.addTextFieldWithConfigurationHandler { (textField) -> Void in
+            textField.placeholder = "Add City Here"
+            }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        alert.addAction(cancelAction)
+        let addAction = UIAlertAction(title: "Add", style: .Default) { (action) -> Void in
+            let cityTextField = alert.textFields![0] as UITextField
+            self.cities.append(Colleges(name: cityTextField.text!))
+            self.tableView.reloadData()
+        }
+        alert.addAction(addAction)
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
 }
 
