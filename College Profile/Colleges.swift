6dc08347082d1a2e0b8c11edd65b1ac5cd51f0b0
Student <student@mkelly2.local>
2016-01-20 11:55:22 -0600

    Finished setting up table view

---
+++ College Profile/Colleges.swift
@@ -0,0 +1,32 @@
+//
+//  Colleges.swift
+//  College Profile
+//
+//  Created by mkelly2 on 1/20/16.
+//  Copyright © 2016 mkelly2. All rights reserved.
+//
+
+import UIKit
+
+class Colleges: NSObject {
+    
+    var name = ""
+    var location = ""
+    var numberOfStudents = 0
+    var image = UIImage(named: "Default")
+    
+    convenience init(name: String, location: String, numberOfStudents: Int, image: UIImage) {
+        self.init()
+        self.name = name
+        self.location = location
+        self.numberOfStudents = numberOfStudents
+        self.image = image
+    }
+    
+    convenience init(name: String) {
+        self.init()
+        self.name = name
+    }
+
+
+}
--- College Profile/ViewController.swift
+++ College Profile/ViewController.swift
@@ -8,16 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var cities: [Colleges] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+ 
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cities.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("MyCell", forIndexPath: indexPath)
+        cell.textLabel?.text = cities[indexPath.row].name
+        return cell
     }
 
 
