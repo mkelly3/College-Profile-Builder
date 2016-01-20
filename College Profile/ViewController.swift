11975cbc457e0471beda17b883dbcc7ece940e47
Student <student@mkelly2.local>
2016-01-20 11:46:01 -0600

    Initial Commit

---
+++ College Profile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  College Profile
+//
+//  Created by mkelly2 on 1/20/16.
+//  Copyright © 2016 mkelly2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
